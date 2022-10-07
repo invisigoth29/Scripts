9f12d7f0e39659da01617bf81eef55bcd5401297
Donovan <donovan.farrell@wnco.com>
2022-10-07 09:30:05 -0500

    new script

---
+++ SMS.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+curl -X POST https://textbelt.com/text \
+     --data-urlencode phone='15635284146' \
+     --data-urlencode message='Your appointment at Unitypoint health is confirmed. If you need to cancel or reschedule, please do so online '  \
+     -d key=d70a5ea9e7b635a001e62d12d7228e8b5357b129ViiQRB3iyEqcRn8Yz47PZUrOt
