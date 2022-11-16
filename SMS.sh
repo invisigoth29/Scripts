24b7926b51f08884243b5db2b2f1bf77b27abb44
invisigoth29 <94144395+invisigoth29@users.noreply.github.com>
2022-11-16 16:38:37 -0600

    Update SMS.sh

--- SMS.sh
+++ SMS.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 curl -X POST https://textbelt.com/text \
-     --data-urlencode phone='15635284146' \
-     --data-urlencode message='Your appointment at Unitypoint health is confirmed. If you need to cancel or reschedule, please do so online '  \
+     --data-urlencode phone='' \
+     --data-urlencode message='Your appointment at is confirmed. If you need to cancel or reschedule, please do so online '  \
      -d key=
