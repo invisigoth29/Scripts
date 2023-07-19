19d7b6334fed3a26262059c523653e9abfa10d94
invisigoth29 <94144395+invisigoth29@users.noreply.github.com>
2023-07-19 14:25:12 -0500

    final commit

--- quickscan.sh
+++ quickscan.sh
@@ -1,38 +1,54 @@
 #!/bin/bash
 # Script to quickly take IP or Domain as input then perform a quick scan
-#
-#
-#
-#
-#export BLUE=$(tput setaf 3 :-"" 2>/dev/null)
-#export RESET=$(tput sgr0 :-"" 2>/dev/null)
-#
-#echo -e "\n $BLUE ____***____________________/\___________________***_______/\____" $RESET
-#echo -e " $BLUE ____***_______WELCOME____***____________/\_________***_____/\________" $RESET
-#echo -e " $BLUE __/\____**____________+_______TO_______________________***______/\______" $RESET
-#echo -e " $BLUE ___________+_____________/\________+_____QUICKSCAN_______/\__+_______" $RESET
-#echo -e " $BLUE ___+____:)_______/\______;)________/\_________:-D_____+__________/\___________\n\n" $RESET
-#
+
+echo -e "\n ____***____________________/\___________________***_______/\____"
+echo -e " ____***_______WELCOME____***____________/\_________***_____/\________"
+echo -e " __/\____**____________+_______TO_______________________***______/\______"
+echo -e " ___________+_____________/\________+_____QUICKSCAN_______/\__+_______"
+echo -e " ___+____:)_______/\______;)__by___Invisigoth29______/\_________:-D_____+__________/\___________\n\n"
+
 echo -e "Welcome to Quickscan"
-echo -e "enter IP to scan"
-read ip
-echo -e "enter domain to scan"
-read domain
-nmap -sV -sC -A -O -oA $ip $domain
-#
-#
-#
-echo "Enter another IP or domain to scan or type exit"
-echo "Enter IP to scan"
-echo "Enter domain to scan"
-echo "Type exit"
-read ip
-read domain
-read exit
-if [ $exit = exit ]
-then
-exit
+echo -e "Enter IP, domain, or integer to scan:"
+read input
 
+# Function to check if the input is a valid IPv4 address
+is_valid_ip() {
+    local ip=$1
+    if [[ $ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+        local IFS='.'
+        local -a octets=($ip)
+        for octet in "${octets[@]}"; do
+            if (( octet < 0 || octet > 255 )); then
+                return 1
+            fi
+        done
+        return 0
+    else
+        return 1
+    fi
+}
 
+# Check if the input is an integer
+if [[ $input =~ ^[0-9]+$ ]]; then
+    echo "Input is an integer. Exiting."
+    exit 1
+fi
+
+# Check if the input is a valid IP address
+if is_valid_ip "$input"; then
+    nmap -Pn -sS -f "$input"  # Scanning an IP address
+else
+    # Assume it's a domain name
+    domain=$input
+    ip=$(dig +short "$domain")  # Use dig command to resolve domain to IP address
+
+    if [ -z "$ip" ]; then
+        echo "Failed to resolve the domain name. Exiting."
+        exit 1
+    fi
+
+    echo "Domain resolved to IP: $ip"
+    nmap -Pn -sS -f  "$domain" "$ip"  # Scanning the domain
+fi
 
 
