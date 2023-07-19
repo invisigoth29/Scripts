ee820203bb6c2d8f6b78d2fa32c5a7d2b308ee35
invisigoth29 <94144395+invisigoth29@users.noreply.github.com>
2023-07-19 11:32:01 -0500

    First commit

---
+++ quickscan.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Script to quickly take IP or Domain as input then perform a quick scan
+#
+#
+#
+#
+echo -e "\n $BLUE ____***____________________/\___________________***_______/\____" $RESET
+echo -e " $BLUE ____***_______WELCOME____***____________/\_________***_____/\________" $RESET
+echo -e " $BLUE __/\____**____________+_______TO_______________________***______/\______" $RESET
+echo -e " $BLUE ___________+_____________/\________+_____QUICKSCAN_______/\__+_______" $RESET
+echo -e " $BLUE ___+____:)_______/\______;)________/\_________:-D_____+__________/\___________\n\n" $RESET
+#
+echo -e "enter IP to scan"
+read ip
+echo -e "enter domain to scan"
+read domain
+nmap -sV -sC -A -O -oA $ip $domain
+#
+#
+#
+echo "Enter another IP or domain to scan or type exit"
+echo "Enter IP to scan"
+echo "Enter domain to scan"
+echo "Type exit"
+read ip
+read domain
+read exit
+if [ $exit = exit ]
+then
+exit
+
+
+
+
